a8b9616fdaf8eaba4660e487c03e28c0bac2c2e1
RakhmatovAmirjon <137805223+RakhmatovAmirjon@users.noreply.github.com>
2024-01-06 14:50:01 +0500

    Update Forms

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let saveChangesButton = document.getElementById("save_changes");
-let important_inputs = document.getElementById("important");
+let every_inputs = document.querySelectorAll("input");
 
-saveChangesButton.onclick = () => {
-  let emptyInputCount = 0;
-
-  for (let input of important_inputs) {
-    if (input.value === "") {
-      input.style.border = "2px solid red";
-      input.parentElement.querySelector(".texting").style.color = "red";
-      emptyInputCount++;
-    } 
-    else {
-      input.style.border = "";
-      input.parentElement.querySelector(".texting").style.color = "";
+every_inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if (input.type === 'text') {
+      if (/[^a-zA-Z]/.test(input.value)) {
+        input.style.border = '2px solid red';
+        input.style.color = 'red';
+      } else {
+        input.style.border = '';
+        input.style.color = 'black';
+      }
+    } else if (input.type === 'number') {
+      if (/[a-zA-Z]/.test(input.value)) {
+        input.style.border = '2px solid red';
+        input.style.color = 'red';
+      } else {
+        input.style.border = '';
+        input.style.color = 'black';
+      }
     }
-  }
-
-  let fourthNumber = document.getElementById("fourth");
-  fourthNumber.textContent = `${emptyInputCount}/12`;
-};
+  });
+});
